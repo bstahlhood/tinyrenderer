64d0a07ba84f1ab5428cb6717d740cc1d97084f8
Ben Stahlhood II <bstahlhood@icloud.com>
2023-08-28 21:25:43 +0700

    (feat): line rendering take 4

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tinyrenderer in Rust for fun :)
+
+Was starting to feel a bit of burn out and whenever I do, I tend to look
+back to my roots of what made me fall in love with coding to begin with:
+
+* Graphics programming
+* Game Development
+* Operating Systems
+
+So this will be a fun distractor for me on weekends. Feel free to fork, follow
+along, or provide feedback if you like.
+
+
+Thanks!
+
+- Ben Stahlhood II
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+//
+//  Implement Tinyrenderer in Rust for fun :)
+//  Original here: https://github.com/ssloy/tinyrenderer
+//
+//  *Note*: Not worrying about being super idiomatic right now.
+//  Mostly doing many iterations as brush strokes toward the end goal.
+//
+//  Currently on this step:
+//  https://github.com/ssloy/tinyrenderer/wiki/Lesson-1:-Bresenham’s-Line-Drawing-Algorithm#fourth-attempt-continued
+//
+
 use softbuffer::{Context, Surface};
 use std::num::NonZeroU32;
 use winit::{
@@ -5,6 +16,53 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
+
+//
+//  Line drawing to frame buffer
+//  https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+//
+fn line(
+    mut x0: i32,
+    mut y0: i32,
+    mut x1: i32,
+    mut y1: i32,
+    buffer: &mut [u32],
+    r: u32,
+    g: u32,
+    b: u32,
+) {
+    let mut steep = false;
+    if (x0 - x1).abs() < (y0 - y1).abs() {
+        std::mem::swap(&mut x0, &mut y0);
+        std::mem::swap(&mut x1, &mut y1);
+        steep = true;
+    }
+    if x0 > x1 {
+        std::mem::swap(&mut x0, &mut x1);
+        std::mem::swap(&mut y0, &mut y1);
+    }
+    let distance_x = x1 - x0;
+    let distance_y = y1 - y0;
+    let distance_error = (distance_y as f64 / distance_x as f64).abs();
+    let mut error = 0.;
+    let mut y = y0;
+    let mut x = x0;
+    while x <= x1 {
+        let color = b | (g << 8) | (r << 16);
+        if steep {
+            buffer[x as usize * 2048 + y as usize] = color;
+        } else {
+            buffer[y as usize * 2048 + x as usize] = color;
+        }
+        error += distance_error;
+        if error > 0.5 {
+            let error_diff = if y1 > y0 { 1 } else { -1 };
+            y += error_diff;
+            error -= 1.;
+        }
+        x += 1;
+    }
+}
 
 fn main() {
     let event_loop = EventLoop::new();
@@ -31,15 +89,10 @@ fn main() {
                     .unwrap();
 
                 let mut buffer = surface.as_mut().unwrap().buffer_mut().unwrap();
-                for index in 0..(width * height) {
-                    let y = index / width;
-                    let x = index % width;
-                    let red = x % 255;
-                    let green = y % 255;
-                    let blue = (x * y) % 255;
 
-                    buffer[index as usize] = blue | (green << 8) | (red << 16);
-                }
+                line(13, 20, 80, 40, &mut buffer, 255, 255, 255);
+                line(20, 13, 40, 80, &mut buffer, 255, 0, 0);
+                line(80, 40, 13, 20, &mut buffer, 255, 0, 0);
 
                 buffer.present().unwrap();
             }
